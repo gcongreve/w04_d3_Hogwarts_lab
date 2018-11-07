bc9db6ff0c3ebfc5c6e90533789c33cf9bb65b67
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-07 19:35:19 +0000

    modified edit, added links to student's on index

--- students_controller.rb
+++ students_controller.rb
@@ -5,18 +5,18 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-get '/students' do
+get '/students/?' do
   @students = Student.all()
   erb(:index)
 end
 
 # new
-get '/students/new' do
+get '/students/new/?' do
   erb(:new)
 end
 
 # create
-post '/students' do
+post '/students/?' do
   @student = Student.new(params)
   @student.save
   redirect to '/students'
@@ -24,13 +24,13 @@ end
 
 
 # show
-get '/students/:id' do
+get '/students/:id/?' do
   @student = Student.find(params[:id])
   erb(:show)
 end
 
 # edit
-get '/students/:id/edit' do
+get '/students/:id/edit/?' do
   @student = Student.find(params[:id])
   #@houses = [1,2,3,4]
 #  @houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
@@ -38,14 +38,14 @@ get '/students/:id/edit' do
 end
 
 # update
-post '/students/:id' do
+post '/students/:id/?' do
   @student = Student.new(params)
   @student.update
   redirect to '/students/' + params[:id]
 end
 
 # destroy
-post '/students/:id/delete' do
+post '/students/:id/delete/?' do
   @student = Student.find(params[:id])
   @student.delete
   redirect to "/students"
